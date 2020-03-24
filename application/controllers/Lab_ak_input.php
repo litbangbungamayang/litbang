d69d905d5d0d2b58af37d9056fa62f1065fcd3bd
Litbang Bungamayang <litbang.bungamayang@gmail.com>
2020-03-25 01:24:23 +0700

    validasi input data analisa

--- application/controllers/Lab_ak_input.php
+++ application/controllers/Lab_ak_input.php
@@ -11,7 +11,8 @@ class Lab_ak_input extends CI_Controller {
 			$this->load->model("dashboard_model");
 			$this->load->helper(array('url', 'html'));
 			$this->load->library('session');
-			$this->simtr_address_local = "http://localhost/SIMTR/index.php/Rdkk_list/test";
+			$this->simpg_address_local = "http://localhost/simpg/index.php";
+			$this->simpg_address_live = "http://simpgbuma.ptpn7.com/index.php";
 			$this->server_env = "LOCAL";
 	}
 
@@ -21,7 +22,6 @@ class Lab_ak_input extends CI_Controller {
     $data['content'] = $this->loadContent();
     $data['script'] = $this->loadScript();
     $this->load->view('main_view', $data);
-		$this->test();
 	}
 
 	public function loadScript(){
@@ -32,28 +32,48 @@ class Lab_ak_input extends CI_Controller {
 		echo $this->dashboard_model->loadData();
 	}
 
-	public function getPetakKebun(){
-
-	}
-
-	public function test(){
-		$cookie_file = 
+	public function getAllPetakKebunByKepemilikan(){
+		$kepemilikan = $this->input->get("kepemilikan");
 		$db_server = "";
 		if($this->server_env == "LOCAL"){
-			$db_server = $this->simtr_address_local;
-		};
+			$db_server = $this->simpg_address_local;
+		} else {
+			$db_server = $this->simpg_address_live;
+		}
 		$curl = curl_init();
     curl_setopt_array($curl, array(
-      CURLOPT_URL => $db_server,
+      CURLOPT_URL => $db_server."/api_buma/getAllPetakKebunByKepemilikan?kepemilikan=".$kepemilikan,
       CURLOPT_RETURNTRANSFER => true,
       CURLOPT_TIMEOUT => 30,
       CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
       CURLOPT_CUSTOMREQUEST => "GET",
       CURLOPT_HTTPHEADER => array(
         "cache-control: no-cache"
-      ),
-			CURLOPT_HTTPAUTH => CURLAUTH_BASIC,
-			CURLOPT_USERPWD => "andes:terong"
+      )
+    ));
+    $response = curl_exec($curl);
+    $error = curl_error($curl);
+    curl_close($curl);
+		echo $response;
+	}
+
+	public function test(){
+		$db_server = "";
+		if($this->server_env == "LOCAL"){
+			$db_server = $this->simpg_address_local;
+		} else {
+			$db_server = $this->simpg_address_online;
+		}
+		$curl = curl_init();
+    curl_setopt_array($curl, array(
+      CURLOPT_URL => $db_server."/api_buma/getAllPetakKebunByKepemilikan",
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_TIMEOUT => 30,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => "GET",
+      CURLOPT_HTTPHEADER => array(
+        "cache-control: no-cache"
+      )
     ));
     $response = curl_exec($curl);
     $error = curl_error($curl);
@@ -81,7 +101,7 @@ class Lab_ak_input extends CI_Controller {
           </div>
           <div class="card-body">
             <div class="row">
-              <div class="col-md-12 col-lg-4">
+              <div class="col-md-12 col-lg-3">
                 <div class="form-group" id="grTahunGiling">
                   <label class="form-label">Tahun Giling</label>
                   <select name="tahun_giling" id="tahun_giling" class="custom-control custom-select" placeholder="Pilih tahun giling">
@@ -94,26 +114,74 @@ class Lab_ak_input extends CI_Controller {
                   <select name="jenis_analisa" id="jenis_analisa" class="custom-control custom-select" placeholder="Pilih jenis analisa"></select>
                   <div class="invalid-feedback">Jenis analisa belum dipilih!</div>
                 </div>
-              </div>
-              <div class="col-md-12 col-lg-4">
                 <div class="form-group" id="grJenisAnalisa">
                   <label class="form-label">Kepemilikan</label>
                   <select name="kepemilikan" id="kepemilikan" class="custom-control custom-select" placeholder="Pilih kepemilikan">
-                    <option value="ts">TS</option>
+										<option value=""></option>
+										<option value="ts">TS</option>
                     <option value="tr">TR</option>
                     <option value="tsi">TSI</option>
                   </select>
                   <div class="invalid-feedback">Kepemilikan tebu belum dipilih!</div>
                 </div>
+							</div>
+              <div class="col-md-12 col-lg-3">
                 <div class="form-group" id="grJenisAnalisa">
                   <label class="form-label">Petak Kebun</label>
-                  <select name="petak_kebun" id="petak_kebun" class="custom-control custom-select" placeholder="Pilih kepemilikan">
-                    <option value="ts">TS</option>
-                    <option value="tr">TR</option>
-                    <option value="tsi">TSI</option>
+                  <select name="petak_kebun" id="petak_kebun" class="custom-control custom-select" placeholder="Pilih petak kebun">
                   </select>
-                  <div class="invalid-feedback">Kepemilikan tebu belum dipilih!</div>
+                  <div class="invalid-feedback">Petak kebun belum dipilih!</div>
                 </div>
+								<div class="form-group" id="grRondeAnalisa">
+                  <label class="form-label">Ronde Analisa</label>
+                  <select name="ronde_analisa" id="ronde_analisa" class="custom-control custom-select" placeholder="Input ronde">
+										<option value=""></option>
+										<option value = 1>1</option>
+										<option value = 2>2</option>
+										<option value = 3>3</option>
+										<option value = 4>4</option>
+										<option value = 5>5</option>
+										<option value = 6>6</option>
+										<option value = 7>7</option>
+										<option value = 8>8</option>
+                  </select>
+                  <div class="invalid-feedback">Ronde analisa belum dipilih!</div>
+                </div>
+								<div class="form-group" id="grTglAnalisa">
+                  <label class="form-label">Tanggal Analisa</label>
+									<input autocomplete="off" type="text" class="form-control text-left" placeholder="Tanggal Awal" id="dtpAwal" style=""></input>
+                  <div class="invalid-feedback">Tanggal analisa belum dipilih!</div>
+                </div>
+								<div class="text-right">
+                    <button id="btnNextAnalisa" type="button" style="margin-right: 30px; width: 200px;" class="btn btn-outline-primary">Lanjutkan</button>
+                </div>
+              </div>
+							<div class="col-md-12 col-lg-6">
+								<div class="card">
+									<div class="card-body">
+	                  <label class="h5">Data Petak Kebun</label>
+										<div class="row">
+											<div class="col-md-12 col-lg-4">
+												<div>Deskripsi Petak</div>
+												<div>Luas (ha)</div>
+												<div>Masa Tanam</div>
+												<div>Varietas</div>
+												<div>Kategori</div>
+												<div><b>Tgl. Analisa Akhir</b></div>
+												<div><b>Ronde Analisa Akhir</b></div>
+											</div>
+											<div class="col-md-12 col-lg-8">
+												<div id="deskripsi_blok"></div>
+												<div id="luas_tanam"></div>
+												<div id="masa_tanam"></div>
+												<div id="varietas"></div>
+												<div id="kategori"></div>
+												<div id="tgl_analisa_akhir"></div>
+												<div id="ronde_analisa_akhir"></div>
+											</div>
+										</div>
+									</div>
+								</div>
               </div>
             </div>
           </div>
--- assets/app_js/Lab_ak_input.js
+++ assets/app_js/Lab_ak_input.js
@@ -1,9 +1,34 @@
 var $cbxTahunGiling;
-var $cbxJenisAnalisa;
-var $kepemilikan;
+var $cbxJenisAnalisa, cbxJenisAnalisa;
+var $cbxKepemilikan, cbxKepemilikan;
 var $cbxPetakKebun, cbxPetakKebun;
+var $cbxRondeAnalisa, cbxRondeAnalisa;
+var dtpAwal = $("#dtpAwal");
+var txtDeskripsiBlok = $("#deskripsi_blok");
+var txtLuasTanam = $("#luas_tanam");
+var txtMasaTanam = $("#masa_tanam");
+var txtVarietas = $("#varietas");
+var txtKategori = $("#kategori");
+var btnNextAnalisa = $("#btnNextAnalisa");
+var $petak_pilihan;
 
+function formatTgl(dateObj){
+  if(dateObj != null){
+    return dateObj.getFullYear() + "-" + ("0" + (dateObj.getMonth()+1)) + "-" + ("0" + dateObj.getDate()).slice(-2);
+  }
+  return "";
+}
 
+function formatTglStr(dateObj){
+  if(dateObj != null){
+    return ("0" + dateObj.getDate()).slice(-2) + "-" + ("0" + (dateObj.getMonth()+1)) + "-" + dateObj.getFullYear();
+  }
+  return "";
+}
+
+$("#dtpAwal").datepicker({
+  format: "dd-MM-yyyy"
+});
 
 $.ajax({
   url: js_base_url + "Lab_ak_input/getJenisAnalisa",
@@ -20,6 +45,7 @@ $.ajax({
       placeholder: "Pilih jenis analisa",
       options: response
     })
+    cbxJenisAnalisa = $cbxJenisAnalisa[0].selectize;
   }
 })
 
@@ -28,17 +54,90 @@ $cbxTahunGiling = $("#tahun_giling").selectize({
   sortField: "text"
 })
 
-$kepemilikan = $("#kepemilikan").selectize({
-  create: false,
-  sortField: "text",
-  maxItems: 1,
-  placeholder: "Pilih kepemilikan tebu"
-})
-
-$cbxPetakKebun = $("#petak_kebun").selectize({
+$cbxRondeAnalisa = $("#ronde_analisa").selectize({
   create: false,
   sortField: "text"
 })
 
+$cbxKepemilikan = $("#kepemilikan").selectize({
+  create: false,
+  sortField: "text",
+  maxItems: 1,
+  placeholder: "Pilih kepemilikan tebu",
+  onChange: function(value){
+    cbxPetakKebun.disable();
+    cbxPetakKebun.clearOptions();
+    cbxPetakKebun.load(function(callback){
+      var $selected = $cbxKepemilikan[0].selectize.getValue();
+      txtDeskripsiBlok.html("");
+      txtLuasTanam.html("");
+      txtMasaTanam.html("");
+      txtVarietas.html("");
+      txtKategori.html("");
+      $.ajax({
+        url: js_base_url + "Lab_ak_input/getAllPetakKebunByKepemilikan",
+        type: "GET",
+        dataType: "json",
+        data: "kepemilikan=" + $selected,
+        success: function(response){
+          cbxPetakKebun.enable();
+          callback(response);
+        }
+      })
+    })
+  }
+})
+
+$cbxPetakKebun = $("#petak_kebun").selectize({
+  create: false,
+  valueField: "kode_blok",
+  labelField: "deskripsi_blok",
+  sortField: "kode_blok",
+  searchField: "deskripsi_blok",
+  placeholder: "Pilih petak kebun",
+  onChange: function(value){
+    $.map(this.items, function(value){
+      $petak_pilihan = cbxPetakKebun.options[value];
+    })
+    txtDeskripsiBlok.html("<b>" + $petak_pilihan.deskripsi_blok + "</b>");
+    txtLuasTanam.html(parseFloat($petak_pilihan.luas_tanam).toLocaleString({maximumFractionDigits: 2}));
+    txtMasaTanam.html($petak_pilihan.periode);
+    txtVarietas.html($petak_pilihan.nama_varietas);
+    txtKategori.html($petak_pilihan.status_blok);
+  }
+})
+
+btnNextAnalisa.on("click", function(){
+  if (validasiForm()){
+    var dataAwalAnalisa = {
+      jenis_analisa: cbxJenisAnalisa.getValue(),
+      tahun_giling: cbxTahunGiling.getValue(),
+      kepemilikan: cbxKepemilikan.getValue(),
+      petak_kebun: $petak_pilihan
+    };
+  }
+})
+
+function validasiForm(){
+  if(cbxTahunGiling.getValue() != "" && cbxJenisAnalisa.getValue() != "" &&
+      cbxKepemilikan.getValue() != "" && cbxPetakKebun.getValue() != "" &&
+      cbxRondeAnalisa.getValue() != "" && dtpAwal.getValue() != ""){
+    $cbxJenisAnalisa.removeClass("is-invalid");
+    $cbxKepemilikan.removeClass("is-invalid");
+    $cbxPetakKebun.removeClass("is-invalid");
+    $cbxRondeAnalisa.removeClass("is-invalid");
+    return true;
+  } else {
+    (cbxJenisAnalisa.getValue() == "") ? $cbxJenisAnalisa.addClass("is-invalid") : $cbxJenisAnalisa.removeClass("is-invalid");
+    (cbxKepemilikan.getValue() == "") ? $cbxKepemilikan.addClass("is-invalid") : $cbxKepemilikan.removeClass("is-invalid");
+    (cbxPetakKebun.getValue() == "") ? $cbxPetakKebun.addClass("is-invalid") : $cbxPetakKebun.removeClass("is-invalid");
+    (cbxRondeAnalisa.getValue() == "") ? $cbxRondeAnalisa.addClass("is-invalid") : $cbxRondeAnalisa.removeClass("is-invalid");
+  }
+  return false;
+}
+
+cbxRondeAnalisa = $cbxRondeAnalisa[0].selectize;
+cbxKepemilikan = $cbxKepemilikan[0].selectize;
+cbxTahunGiling = $cbxTahunGiling[0].selectize;
 cbxPetakKebun = $cbxPetakKebun[0].selectize;
 cbxPetakKebun.disable();
